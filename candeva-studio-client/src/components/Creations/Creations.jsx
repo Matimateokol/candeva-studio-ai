bda08d948ef126d9e08526211a690b2510dbf170
Matimateokol <58779750+Matimateokol@users.noreply.github.com>
2024-06-08 18:21:17 +0200

    Refactor code and apply propTypes validation for CreditsButton component

--- candeva-studio-client/src/components/Creations/Creations.jsx
+++ candeva-studio-client/src/components/Creations/Creations.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './Creations.css';
 
 export default function Creations() {
--- candeva-studio-client/src/components/CreditsButton.jsx
+++ candeva-studio-client/src/components/CreditsButton.jsx
@@ -22,3 +22,7 @@ export default function CreditsButton({ credits }) {
     </button>
   );
 }
+
+CreditsButton.propTypes = {
+  credits: Number,
+};
